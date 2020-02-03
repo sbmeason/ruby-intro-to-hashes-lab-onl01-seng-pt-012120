4cff7cbc9d930cc51dbb725b9911dda944c0dbcb
Samantha Meason <sbmeason@gmail.com>
2020-02-03 19:01:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,13 @@ monopoly =  {
       :three_pieces_owned=>100,
       :four_pieces_owned=>200
     },
-
+ :names=>
+    {
+      :reading_railroad=>{},
+      :pennsylvania_railroad=>{},
+      :b_and_o_railroad=>{},
+      :shortline=>
+      {}
 end
 
 def monopoly_with_fourth_tier
