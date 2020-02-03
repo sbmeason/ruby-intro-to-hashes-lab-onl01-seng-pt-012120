597f2125880834079fe86f11d7bafdf33981754b
Samantha Meason <sbmeason@gmail.com>
2020-02-03 17:58:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,9 @@ actor = {name: "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = {:railroads =>}
+	monopoly = {:railroads =>
+	  
+	}
 
 end
 
@@ -54,4 +56,5 @@ monopoly =  {
     }
   }
 }
+end
 end
